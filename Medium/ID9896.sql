6f28ca31ef9ef93ca11d0aa8796e7d5c58d84c20
Karthick Krishna Balaji <42288304+krishna1194@users.noreply.github.com>
2022-11-07 01:35:59 -0500

    Solutions for Medium problems

--- Medium/ID9896.sql
+++ Medium/ID9896.sql
@@ -3,5 +3,5 @@
 # Problem Detail:Find customers who have never made an order. Output the first name of the customer.
 
 # Code:
-select first_name from customers
+select first_name from customers 
 where id not in (select cust_id from orders);
