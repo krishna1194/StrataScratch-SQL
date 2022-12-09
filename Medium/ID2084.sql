07de5c8f3c4210a62108fa10ecaad5679995bac0
Karthick Krishna Balaji <42288304+krishna1194@users.noreply.github.com>
2022-12-08 20:25:24 -0500

    Solutions for Medium problems

---
+++ Medium/ID2084.sql
@@ -0,0 +1,12 @@
+# Problem Name: Blocked Users
+# Problem Id: ID 2084
+# Problem Detail: You are given a table of users who have been blocked from Facebook, together with the date, duration, and the reason for the blocking. The duration is expressed as the number of days after blocking date and if this field is empty, this means that a user is blocked permanently. For each blocking reason, count how many users were blocked in December 2021. Include both the users who were blocked in December 2021 and those who were blocked before but remained blocked for at least a part of December 2021.
+
+# Code:
+select block_reason, count(distinct user_id) as n_users
+from fb_blocked_users
+where  
+(year(block_date) = '2021' and month(block_date) = '12') or
+(block_date < '2021-12-01' and (block_duration is null or
+(datediff('2021-12-01', block_date) <= block_duration)))
+group by 1;
